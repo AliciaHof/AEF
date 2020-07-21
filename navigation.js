0df76631093c46784b448538327d9099e0492897
AliciaHof <alicia.hofstaetter@geogebra.org>
2020-07-21 14:38:37 +0200

    header + intro page

--- navigation.js
+++ navigation.js
@@ -1,7 +1,7 @@
 
 /* Lists of resource collections to load when clicking through map */
-var idList = ['RHYH3UQ8','tvvekv46','chs4jfvs', 'ts2adk9c', 'XUv5mXTm'];
-var idListOrigami = ['mm43NFSQ','vrCQedkw','tT6Yj7Dg'];
+var idList = ['pmtx7jvh','f2sjzykh','chs4jfvs', 'ts2adk9c', 'XUv5mXTm'];
+var idListOrigami = ['xmxtuapd','wmq7a7hx','xkmqq4m6','j3t2wceu','kegj5vy8','jfaaar2m','mgk2chdc'];
 
 
  if (window.addEventListener){
@@ -16,8 +16,8 @@ function createCardSet(list){
 	if(document.getElementById("cardList")){
 		document.getElementById("cardList").innerHTML = ' ';
 	}
-	for(var i=0; i<list.length; i++){
-		createCard(list[i]);
+	for (let id of list){
+		createCard(id);
 	}
 }
 
@@ -25,40 +25,40 @@ function createCardSet(list){
 function createCard(id) {
   var xhttp = new XMLHttpRequest();
   
-  xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-   var response = xhttp.responseText;
-  
-  if (response) {
-	response = JSON.parse(response);
-	var filename = response.title;
-	var previewUrl = response.thumbUrl;
-	// seems to be a bug in api -> returns "...$1.png" instead of "...@1.png"
-	var previewImg = previewUrl.substr(0,previewUrl.length-6) + '@l.png';
-	var type = response.type;
-	var author = response.creator.displayname;
-	
-  }
-  
-  var typeText = type=='ws'? "Activity" : "Book";
-  
-  var card = 
-		'<div class="card"><a class="card-link" title="'+ filename +'" href="https://www.geogebra.org/m/'+ id +'" target="_blank">'+
-			'<div class="card-image" id="card-image"><img alt="" src="' + previewImg +'" ></div>'+
-			'<div class="card-content">'+
-				'<h3 class="card-title" id="card-title">'+ filename +'</h3>'+
-				'<div class="card-type" id="card-type">'+ typeText +'</div>'+
-				'<p class="card-author" id=="card-author">'+ author +'</p>'+
-			'</div></a></div>';
-  var cardNode = document.createElement("LI");
-  cardNode.innerHTML = card;
-  document.getElementById("cardList").appendChild(cardNode);
-  }
-};
-  /* GeoGebra Website API call */
+   /* GeoGebra Website API call */
   xhttp.open("GET", "https://api.geogebra.org/v1.0/materials/" + id + "?scope=basic&embed=creator", true);
   xhttp.send();
- 
+  
+  xhttp.onreadystatechange = function() {
+	  if (this.readyState == 4 && this.status == 200) {
+	   var response = xhttp.responseText;
+	  
+	  if (response) {
+		response = JSON.parse(response);
+		var filename = response.title;
+		var previewUrl = response.thumbUrl;
+		// seems to be a bug in api -> returns "...$1.png" instead of "...@1.png"
+		var previewImg = previewUrl.substr(0,previewUrl.length-6) + '@l.png';
+		var type = response.type;
+		var author = response.creator.displayname;
+		
+	  }
+	  
+	  var typeText = type=='ws'? "Activity" : "Book";
+	  
+	  var card = 
+			'<div class="card"><a class="card-link" title="'+ filename +'" href="https://www.geogebra.org/m/'+ id +'" target="_blank">'+
+				'<div class="card-image" id="card-image"><img alt="" src="' + previewImg +'" ></div>'+
+				'<div class="card-content">'+
+					'<h3 class="card-title" id="card-title">'+ filename +'</h3>'+
+					'<div class="card-type" id="card-type">'+ typeText +'</div>'+
+					'<p class="card-author" id=="card-author">'+ author +'</p>'+
+				'</div></a></div>';
+	  var cardNode = document.createElement("LI");
+	  cardNode.innerHTML = card;
+	 document.getElementById("cardList").appendChild(cardNode);
+	  }
+	}; 
 }
 
 /* set node active for correct css */
